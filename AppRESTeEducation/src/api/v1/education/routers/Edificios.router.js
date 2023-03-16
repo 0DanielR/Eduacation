8c33e68204f0669b96a74223d5b948b48f937ed2
0DanielR <102000459+0DanielR@users.noreply.github.com>
2023-03-15 19:41:28 -0600

    Add files via upload

---
+++ AppRESTeEducation/src/api/v1/education/routers/Edificios.router.js
@@ -0,0 +1,15 @@
+import { Router } from "express";
+import * as Edificiocontroller from '../controllers/Edificio.controller';
+
+const router = Router(); 
+
+router.get('/', Edificiocontroller.getEdificiosList); 
+
+router.get('/:id', Edificiocontroller.getEdificioItem); 
+export default router;
+
+router.post('/', Edificiocontroller.postEdificioItem);
+
+router.put('/:id', Edificiocontroller.putEdificioItem);
+
+router.delete('/:id', Edificiocontroller.deleteEdificioItem);  
