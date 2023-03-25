2f9783d2d75cb64b8d6e99661b992f51f10312a1
0DanielR <102000459+0DanielR@users.noreply.github.com>
2023-03-25 01:08:04 -0600

    Delete AppRestEducation directory

--- AppRestEducation/src/api/v1/education/routers/Periodos.router.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express'; 
-import * as periodosController from '../controllers/Periodos.controller'; 
-const router = Router(); 
-router.get('/', periodosController.getPeriodosList);
-router.get('/:id', periodosController.getPeriodosItem); 
-router.post('/', periodosController.postPeriodosItem); 
-router.put('/:id', periodosController.putPeriodosItem);
-router.delete('/:id', periodosController.deletePeriodosItem);  
-export default router;
